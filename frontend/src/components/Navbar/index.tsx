feb710ab2581f7e5dc29ef3c7239125f62363b7c
Tiago Silva <programtiago@gmail.com>
2022-05-14 11:53:40 +0100

    Estilizacao da Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,15 +1,16 @@
 import {ReactComponent as GithubIcon} from 'assets/img/icons8-github.svg';
+import './styles.css';
 
 function NavBar(){
     return(
         <header>
-        <nav>
-          <div>
+        <nav className="container">
+          <div className="dsmovie-nav-content">
             <h1>DSMovie</h1>
             <a href="https://github.com/programtiago">
-              <div>
+              <div className="dsmovie-contact-container">
                 <GithubIcon />
-                <p>/programtiago</p>
+                <p className="dsmovie-contact-link">/programtiago</p>
               </div>
             </a>
           </div>
