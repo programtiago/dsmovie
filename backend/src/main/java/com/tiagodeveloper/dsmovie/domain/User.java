2b9f9831ae767329a8e9c5ef74d7e13122798411
Tiago Silva <programtiago@gmail.com>
2022-05-14 14:15:54 +0100

    Movie, User, ScorePK and User domains created

---
+++ backend/src/main/java/com/tiagodeveloper/dsmovie/domain/User.java
@@ -0,0 +1,25 @@
+package com.tiagodeveloper.dsmovie.domain;
+
+public class User {
+
+    private Long id;
+    private String email;
+
+    public User(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
